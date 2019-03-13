474180ae49e6c0b1dc425e92b684c2817fdc4d60
carloHan <hanying313@gmail.com>
2019-03-13 06:50:55 +0100

    Reference parameter function
    The purpose of this function is to find the product of any number.

---
+++ exercise_2.py
@@ -0,0 +1,10 @@
+def product(*numbers):
+    result = 1
+    for n in numbers:
+        result = result * n
+    return result
+
+print('product(5) =', product(5))
+print('product(5, 6) =', product(5, 6))
+print('product(5, 6, 7) =', product(5, 6, 7))
+print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
