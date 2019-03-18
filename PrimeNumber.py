9d28d6866eaa1437104e87565c1d7e8d490d717b
carloHan <hanying313@gmail.com>
2019-03-18 01:37:37 +0100

    Update PrimeNumber.py

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -21,3 +21,10 @@ for n in primes():
         print(n)
     else:
         break
+       
+# Palindrome filter
+
+def is_palindrome(n):
+    return str(n) == str(n)[: : -1]
+output = filter(is_palindrome, range(1, 1000))
+print(list(output))
